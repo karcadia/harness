4d3cad97e92e89d4931eb9126c6036c7c9c654e6
Justin McCormick <karcadia@gmail.com>
2024-06-05 13:47:52 -0500

    Publish 0.1.6 with another fix for spec sanitation in legacy_connectors.

--- plugins/modules/legacy_connector.py
+++ plugins/modules/legacy_connector.py
@@ -193,6 +193,8 @@ def ensure_present(module):
           del existing[module.object_type]['spec']['auth']['spec']['username']
         if 'usernameRef' in existing[module.object_type]['spec']['auth']['spec'].keys() and 'usernameRef' not in pre_json_object[module.object_type]['spec']['auth']['spec'].keys():
           del existing[module.object_type]['spec']['auth']['spec']['usernameRef']
+      if 'credential' in existing[module.object_type]['spec'].keys() and 'spec' in existing[module.object_type]['spec']['credential'].keys() and existing[module.object_type]['spec']['credentials']['spec'] is None:
+        del existing[module.object_type]['spec']['credential']['spec']
       # Now compare the requested spec with the sanitized existing spec.
       if pre_json_object[module.object_type]['spec'] != existing[module.object_type]['spec']:
         needs_update = True
