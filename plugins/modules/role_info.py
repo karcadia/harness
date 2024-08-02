6b8a8fb3c46b2589b246228a1efb6cb9bc1d3795
Justin McCormick <karcadia@gmail.com>
2024-08-02 14:27:51 -0500

    Publish 0.5.5 with a working role_info module.

--- plugins/modules/role_info.py
+++ plugins/modules/role_info.py
@@ -6,52 +6,52 @@
 
 DOCUMENTATION = r"""
 ---
-module: env_info
-version_added: 0.2.2
-short_description: Retrieve environment information from Harness for the given scope.
+module: role_info
+version_added: 0.5.5
+short_description: Retrieve role information from Harness for the given scope.
 description:
-  - Retrieve information for Harness Environments.
+  - Retrieve information for Harness Roles.
   - Can be used at account, org, or project scope.
 author:
   - Justin McCormick (@karcadia)
 options:
   org:
-    description: Identifier of the Harness Organization from which the environment list should be pulled.
+    description: Identifier of the Harness Organization from which the role list should be pulled.
     required: False
     type: str
   project:
-    description: Identifier of the Harness Project from which the environment list should be pulled.
+    description: Identifier of the Harness Project from which the role list should be pulled.
     required: False
     type: str
 """
 
 EXAMPLES = r"""
-- name: List Harness account-level environments.
-  karcadia.harness.env_info:
+- name: List Harness account-level roles.
+  karcadia.harness.role_info:
 
-- name: List Harness org-level environments.
-  karcadia.harness.env_info:
+- name: List Harness org-level roles.
+  karcadia.harness.role_info:
     org: my_demo_org
 
-- name: List Harness project-level environments.
-  karcadia.harness.env_info:
-    org: my_demo_org
-    project: my_demo_project
-  register: env_list
-
-- name: List Harness project-level environments.
-  karcadia.harness.env_info:
+- name: List Harness project-level roles.
+  karcadia.harness.role_info:
     org: my_demo_org
     project: my_demo_project
-  register: env_list
+  register: role_list
+
+- name: List Harness project-level roles.
+  karcadia.harness.role_info:
+    org: my_demo_org
+    project: my_demo_project
+  register: role_list
   environment:
     HARNESS_ACCOUNT_ID: abc123
     HARNESS_API_KEY: abc123
 """
 
 RETURN = r"""
-environments:
-  description: List of environments found in scope.
+roles:
+  description: List of roles found in scope.
   type: list
 """
 
@@ -66,42 +66,45 @@ from yaml import dump
 # External Imports
 from requests import request
 
-def fetch_environments(module, org_id, project_id):
-  # Fetch environments for project.
+def fetch_roles(module, org_id, project_id):
+  # Fetch roles for project.
   page = 0
   page_limit = 20
   account_id = module.headers['Harness-Account']
-  url = f'https://app.harness.io/ng/api/environmentsV2?accountIdentifier={account_id}&sort=name'
-  if org_id:
-    url += f'&orgIdentifier={org_id}'
-  if project_id:
-    url += f'&projectIdentifier={project_id}'
-  url += f'&size={page_limit}'
+  url = module.url
+  url += f'&limit={page_limit}'
   url_without_page = url
   url += f'&page={page}'
-  env_list_resp = request("GET", url, headers=module.headers)
+  role_list_resp = request("GET", url, headers=module.headers)
 
   # Interpret the API response.
-  if env_list_resp.status_code == 200:
+  if role_list_resp.status_code == 200:
     # Check if we got all the items on the first call.
-    env_list = loads(env_list_resp.text)['data']['content']
-    resp_headers = env_list_resp.headers
-    while 'Content-Length' not in resp_headers.keys():
+    role_list = loads(role_list_resp.text)
+    resp_headers = role_list_resp.headers
+    page_number = int(resp_headers['X-Page-Number'])
+    items_so_far = (page_number + 1) * page_limit
+    total_elements = int(resp_headers['X-Total-Elements'])
+    while items_so_far < total_elements:
       # Keep calling until we have everything.
       page += 1
       url = url_without_page
       url += f'&page={page}'
-      env_list_resp = request("GET", url, headers=module.headers)
-      env_list.extend(loads(env_list_resp.text)['data']['content'])
-      resp_headers = env_list_resp.headers
+      role_list_resp = request("GET", url, headers=module.headers)
+      role_list.extend(loads(role_list_resp.text))
+      resp_headers = role_list_resp.headers
+      page_number = int(resp_headers['X-Page-Number'])
+      items_so_far = (page_number + 1) * page_limit
+      total_elements = int(resp_headers['X-Total-Elements'])
   else:
     # Try to extract the status_code to return with our failure.
-    status_code = str(env_list_resp.status_code)
+    status_code = str(role_list_resp.status_code)
     msg=[]
-    msg.append(f'Harness Environment List Response was unexpected. Status Code: {status_code}')
+    msg.append(f'Harness Role List Response was unexpected. Status Code: {status_code}')
+    msg.append(role_list_resp.text)
     module.fail_json(msg=msg)
 
-  module.exit_json(changed=False, environments=env_list)
+  module.exit_json(changed=False, roles=role_list)
 
 def main():
     # Initialize the module and specify the argument spec.
@@ -116,7 +119,7 @@ def main():
     )
 
     # Set the object type for this module.
-    module.object_type = 'environment'
+    module.object_type = 'role'
     module.object_title = module.object_type.title()
 
     # Catch and fail when we were given an ID with a dash in it.
@@ -156,8 +159,24 @@ def main():
     headers['Content-Type'] = 'application/json'
     module.headers = headers
 
+    # Determine the scope of our object.
+    if org_id and project_id:
+      module.object_scope = 'project'
+    elif org_id:
+      module.object_scope = 'org'
+    else:
+      module.object_scope = 'account'
+
+    # Prepare the Harness API URLs for this module.
+    if module.object_scope == 'account':
+      module.url = f'https://app.harness.io/v1/{module.object_type}s?sort=identifier&order=ASC'
+    elif module.object_scope == 'org':
+      module.url = f'https://app.harness.io/v1/orgs/{org_id}/{module.object_type}s?sort=identifier&order=ASC'
+    else:
+      module.url = f'https://app.harness.io/v1/orgs/{org_id}/projects/{project_id}/{module.object_type}s?sort=identifier&order=ASC'
+
     # Call the backup function.
-    fetch_environments(module, org_id, project_id)
+    fetch_roles(module, org_id, project_id)
 
 if __name__ == "__main__":
     main()
